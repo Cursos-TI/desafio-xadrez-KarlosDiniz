4c2cd7159a5a7952957938f9c3f19d9ac6d19ca6
KarlosDiniz <karlos.diniz74@gmail.com>
2025-02-08 20:06:00 +0000

    Finalizado Desafio Xadrez Nivel Novato

--- xadrez.c
+++ xadrez.c
@@ -28,5 +28,43 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
+    //Iniciando o código do Desafio Xadrez - Nivel Novato - Movimentação das peças:
+    // - Bispo: 5 casas na diagonal superior direita
+    // - Torre: 5 casas para a direita
+    // - Rainha: 8 casas para a esquerda
+
+    int cima, baixo, direita, esquerda;
+
+    // movimento do Bispo limitado a 5 casas sendo cima e direita para um movimento na diagonal
+    printf("\nIniciando a movimentação do Bispo: 5 casas na diagonal superior direita:\n");
+    cima = 1;
+    direita = 1;
+    while (cima <=5)
+    {
+        printf("Movimento do Bispo para %dª casa para cima e %dª casa para direita\n", cima, direita);
+        printf("Cima\n");
+        printf("Direita\n");
+        cima++;
+        direita++;
+    }
+    // movimento do Torre limitado a 5 casas para a direita
+    printf("\nIniciando a movimentação da Torre 5 casas para a direita:\n");
+    direita = 1;
+    do
+    {
+        printf("Movimento da Torre para %dª casa à direita\n", direita);
+        printf("Direita\n");
+        direita++;
+    } while (direita <=5);
+
+    // movimento da Rainha limitado a 8 casas para a esquerda
+    printf("\nIniciando a movimentação da Rainha: 8 casas para a esquerda:\n");
+    esquerda = 1;
+    for (esquerda; esquerda <=8; esquerda++)
+    {
+        printf("Movimento da Rainha para %dª casa à esquerda\n", esquerda);
+        printf("Esquerda\n");
+    }
+    printf("\n"); // apenas para dar uma espaço de linha a mais no final
     return 0;
 }
